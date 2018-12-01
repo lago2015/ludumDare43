0d74754fc9404995b722d2a6119c35808bb2bef5
Douglas1701 <matt.w.doug@gmail.com>
2018-12-01 15:47:38 -0500

    Menu System Up

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuScreen;
+    public GameObject optionScreen;
+
+    public Slider sfxSlider;
+    public Slider musicSlider;
+
+    public Text startText;
+    public Text titleText;
+
+    private bool menuOn;
+    private SoundManager soundManger;
+
+    private void Start()
+    {
+        Menu();
+        soundManger = FindObjectOfType<SoundManager>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(!menuOn)
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        menuScreen.SetActive(false);
+        menuOn = false;
+        Time.timeScale = 1;
+    }
+
+    public void Options()
+    {
+        sfxSlider.value = soundManger.effects.volume;
+        musicSlider.value = soundManger.music.volume;
+        optionScreen.SetActive(true);
+    }
+
+    public void Back()
+    {
+        optionScreen.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        menuScreen.SetActive(true);
+        optionScreen.SetActive(false);
+        menuOn = true;
+        Time.timeScale = 0;
+    }
+
+    public void Pause()
+    {
+        Menu();
+        startText.text = "Resume";
+    }
+
+    public void GameOver()
+    {
+        Menu();
+        titleText.text = "Game Over";
+        startText.text = "Restart";
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void setVolume(bool sfx)
+    {
+        if (sfx)
+        {
+            soundManger.SetSFXVolume(sfxSlider.value);
+        }
+        else
+        {
+            soundManger.SetMusicVolume(musicSlider.value);
+        }
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct Sound
+{
+    public string name;
+    public AudioClip clip;
+};
+
+public class SoundManager : MonoBehaviour
+{
+    // Audio sources for sound effects and music
+    public AudioSource effects;
+    public AudioSource music;
+
+    // Array of sounds to use
+    public Sound[] sounds;
+
+    // Dictionary to hold reference to all the sounds
+    private Dictionary<string, AudioClip> soundList;
+
+    private void Start()
+    {
+        soundList = new Dictionary<string, AudioClip>();
+
+        foreach (Sound sound in sounds)
+        {
+            soundList.Add(sound.name, sound.clip);
+        }
+    }
+
+    public void PlaySound(string clip)
+    {
+        effects.clip = soundList[clip];
+        effects.Play();
+    }
+
+    public void PlayMusic(string clip)
+    {
+        music.clip = soundList[clip];
+        music.Play();
+    }
+
+    public void SetSFXVolume(float val)
+    {
+        effects.volume = val;
+    }
+
+    public void SetMusicVolume(float val)
+    {
+        music.volume = val;
+    }
+}
