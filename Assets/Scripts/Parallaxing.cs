d5a4563780456ae547e8ef1507f0f1bcae873011
Douglas1701 <matt.w.doug@gmail.com>
2018-12-01 23:52:41 -0500

    Background parallaxing

---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour {
+
+    public float speed = -1;
+    public float cuttOffPoint = -23.5f;
+    public GameObject spawnpoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(new Vector3(Time.deltaTime * speed, 0.0f));
+        if (transform.position.x < cuttOffPoint)
+        {
+            transform.position = spawnpoint.transform.position;
+        }
+;	}
+}
